c54f62e59e1d5115559d3a7f6a9e1c0c2bbf7e1d
ricardo guerrero <richi_gro@hotmail.com>
2022-04-13 21:53:43 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+const SELECTABLE_COLORS: { label: string; value: string }[] = [
+  { label: "Red", value: "#F55353" },
+  { label: "Blue", value: "#035397" },
+  { label: "Green", value: "#6BCB77" },
+  { label: "Orange", value: "#FF8C32" },
+  { label: "Pink", value: "#F900BF" },
+];
 
-function App() {
+const MainLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const ColorContainer = styled.div`
+  margin-top: 1rem;
+  border-radius: 4px;
+  border: 2px solid black;
+  width: 50%;
+  padding: 2rem;
+  display: flex;
+`;
+
+const ColorButton = styled.button<{ color?: string }>`
+  background: ${({ color }) => (color ? color : "white")};
+  width: 6rem;
+  height: 6rem;
+  border-radius: 4px;
+  margin-right: 2rem;
+  border: none;
+  color: white;
+`;
+
+const StyledSpan = styled.span`
+  background: black;
+  padding: 0.5rem;
+  opacity: 0.9rem;
+`;
+
+const App = () => {
+  const [favoriteColor, setFavoriteColor] = React.useState("red");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainLayout>
+      <h1>My favorite color is {favoriteColor}</h1>
+      <div>Click one of the buttons below to select your favorite color: </div>
+      <ColorContainer>
+        {SELECTABLE_COLORS.map((color) => {
+          return (
+            <ColorButton
+              key={color.label}
+              color={color.value}
+              onClick={() => {
+                console.log("your favorite color is now: ", color.label);
+              }}
+            >
+              <StyledSpan>{color.label}</StyledSpan>
+            </ColorButton>
+          );
+        })}
+      </ColorContainer>
+    </MainLayout>
   );
-}
+};
 
 export default App;
