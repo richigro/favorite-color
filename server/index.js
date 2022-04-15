a989c02ca7b43be02175cb5815f1e8deb9a6b1f4
ricardo guerrero <richi_gro@hotmail.com>
2022-04-14 19:02:34 -0700

    allow cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const COLORS = require("./colors.json");
 const port = 5000;
+
+app.use(cors());
 
 app.get("/colors", (req, res) => {
   res.status(201).json(COLORS);
